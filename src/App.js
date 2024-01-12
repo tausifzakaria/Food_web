0ab274cebea4e2dc02591b68b6709b65335ef069
tausifzakaria <zakariatausif2@gmail.com>
2024-01-12 17:31:41 +0530

    Folder Structure Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className='bg-dark vh-100'></div>
+    <>
+    </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import HomePage from "./HomePage/HomePage";
+import CategoryPage from "./CategoryPage/CategoryPage";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import MealDetailPage from "./MealDetailPage/MealDetailPage";
+
+export {
+    HomePage,
+    CategoryPage,
+    ErrorPage,
+    MealDetailPage,
+}
